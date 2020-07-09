bdb04f2dbc77d5433c06e280d632ea2b80f64af5
Ryan Simmons <simmonsrgw@gmail.com>
2020-07-09 00:04:21 -0400

    Got initial log in working

--- app/src/main/java/io/github/ryangwsimmons/wamobile/MainActivity.kt
+++ app/src/main/java/io/github/ryangwsimmons/wamobile/MainActivity.kt
@@ -1,11 +1,63 @@
 package io.github.ryangwsimmons.wamobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set the action for when the "Login" button is tapped
+        button_login.setOnClickListener(fun (v: View) {
+            //Make the bad login text invisible
+            textView_badLogin.visibility = View.INVISIBLE
+
+            //Get the username and password the user entered
+            var username: String = editText_username.text.toString()
+            var password: String = editText_password.text.toString()
+
+            //Make sure that a username and password have been entered
+            if (username == "" || password == "") {
+                textView_badLogin.text = "Please enter a valid username and password."
+                textView_badLogin.visibility = View.VISIBLE
+
+                return
+            }
+
+            //Create a new WebAdvisor session
+            val session: WASession = WASession(username, password)
+
+            try {
+                CoroutineScope(IO).launch {
+                    //Initiate the connection to WebAdvisor
+                    if (session.initConnection() != "success") {
+                        throw Exception("A network error has occurred. Please check your internet connection and try again.")
+                    }
+
+                    //Change the activity
+                    withContext(Main) {
+                        //Create an intent to change activity
+                        val intent = Intent(this@MainActivity, UserMenuActivity::class.java).apply {
+                            putExtra("session", session)
+                        }
+
+                        //Start the activity
+                        startActivity(intent)
+                    }
+                }
+            } catch (e: Exception) {
+                textView_badLogin.text = e.message
+                textView_badLogin.visibility = View.VISIBLE
+            }
+        })
     }
 }
---
+++ app/src/main/java/io/github/ryangwsimmons/wamobile/WASession.kt
@@ -0,0 +1,94 @@
+package io.github.ryangwsimmons.wamobile
+
+import android.os.AsyncTask
+import org.jsoup.Connection.Method
+import org.jsoup.Connection.Response
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import java.io.Serializable
+
+import kotlin.collections.Map
+
+class WASession(private val username: String, private val password: String): Serializable {
+    //Map that always holds the cookies for the home page of WebAdvisor, so that other various pages can be visited
+    lateinit var homeCookies: Map<String, String>
+
+    public suspend fun initConnection(): String {
+        suspend fun makeInitialConnection(): Map<String, String> {
+            //Make the initial connection to the WebAdvisor main page
+            var res: Response = Jsoup.connect("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?TYPE=M&PID=CORE-WBMAIN&TOKENIDX=").followRedirects(true).execute()
+
+            //Collect the cookies from the previous connection
+            var cookies: Map<String, String> = res.cookies()
+
+            return cookies
+        }
+
+        suspend fun makeLoginConnection(cookies: Map<String, String>): Response {
+            //Make the login request to the server
+            val res = Jsoup.connect("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?TOKENIDX="
+                    + cookies.get("LASTTOKEN")
+                    + "&SS=LGRQ&URL=https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?TYPE=M%26PID=CORE-WBMAIN%26TOKENIDX="
+                    + cookies.get("LASTTOKEN")
+                    + "%26WARN=Y")
+                .data("USER.NAME", this.username, "CURR.PWD", this.password, "RETURN.URL", "https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?TYPE=M&PID=CORE-WBMAIN", "SUBMIT_OPTIONS", "")
+                .cookies(cookies)
+                .method(Method.POST)
+                .followRedirects(true)
+                .execute()
+
+            return res
+        }
+
+        try {
+            var cookies = makeInitialConnection()
+            val res = makeLoginConnection(cookies)
+
+            //Parse the resulting document
+            var doc: Document = res.parse()
+
+            //Check that the series of connections resulted in the user getting the main page, and not the log in page or some other page
+            var docTitle: String = doc.getElementsByTag("title").text()
+            if (!(docTitle.contains("Main Menu"))) {
+                return "Unable to log into WebAdvisor. Check that your login credentials are correct, and that you are connected to the internet."
+            }
+
+            //Copy the cookies from the home page into the homeCookies property
+            this.homeCookies = cookies.toMap()
+
+            return "success"
+        } catch (e: Exception) {
+            return e.message ?: "A network error has occured. Please check your internet connection and try again."
+        }
+    }
+
+    public suspend fun getName(): String {
+        try {
+            //Connect to the WebAdvisor main page
+            var res: Response = Jsoup.connect("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?TYPE=M&PID=CORE-WBMAIN&TOKENIDX=" + this.homeCookies.get("LASTTOKEN"))
+                                     .cookies(this.homeCookies)
+                                     .followRedirects(true)
+                                     .execute()
+
+            //Parse the response document's HTML
+            var doc: Document = res.parse()
+
+            //Get the name
+            var name: String = doc.getElementById("global")
+                                  .getElementsByClass("container").get(0)
+                                  .getElementsByTag("h1").get(0)
+                                  .getElementsByClass("department").get(0)
+                                  .getElementsByTag("a").text()
+                                  .substring(15)
+
+            //Return the name
+            return name
+        } catch (e: Exception) {
+            return e.message ?: "DATA REQUEST FAILED"
+        }
+
+        return "DATA REQUEST FAILED"
+    }
+}
