023357d92dcff217d72514f7b82ef6db93a608c4
Ryan Simmons <simmonsrgw@gmail.com>
2021-08-03 20:19:27 -0400

    Added about dialog to navigation menu

--- app/src/main/java/ca/ryangwsimmons/wamobile/BaseActivity.kt
+++ app/src/main/java/ca/ryangwsimmons/wamobile/BaseActivity.kt
@@ -2,12 +2,20 @@
 
 package ca.ryangwsimmons.wamobile
 
+import android.app.AlertDialog
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +30,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
 
 class BaseActivity : AppCompatActivity() {
 
@@ -44,36 +53,36 @@ class BaseActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        //Get the session object
+        // Get the session object
         val bundle: Bundle = intent.getBundleExtra("bundle")!!
         this.session = bundle.getParcelable("session")!!
 
-        //Define the toolbar for the activity
+        // Define the toolbar for the activity
         this.toolbar = this.binding.layoutToolbar.toolBar
         setSupportActionBar(toolbar)
 
-        //Get other layout items
+        // Get other layout items
         this.progressBar = this.binding.progressBar
         this.fragmentContainer = this.binding.fragmentContainer
 
-        //Set properties for the ActionBar in the activity
+        // Set properties for the ActionBar in the activity
         this.actionBar = supportActionBar!!
         actionBar.setDisplayHomeAsUpEnabled(true)
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu)
 
-        //Define the DrawerLayout, FragmentManager, NavMenuItemListener, and NavigationView for the activity
+        // Define the DrawerLayout, FragmentManager, NavMenuItemListener, and NavigationView for the activity
         this.drawerLayout = this.binding.drawerLayout
         this.fragmentManager = supportFragmentManager
         this.navListener = NavMenuItemListener(session, this.fragmentManager, this.drawerLayout, actionBar, this.progressBar, this.fragmentContainer)
         this.navigationView = this.binding.navigationView
 
-        //Set the listeners for the NavigationView
+        // Set the listeners for the NavigationView
         this.navigationView.setNavigationItemSelectedListener(this.navListener)
 
-        //Set the default selection for the navigation drawer to "News"
+        // Set the default selection for the navigation drawer to "News"
         this.navListener.fragmentSwitchAction(this.navigationView.menu.getItem(0))
 
-        //Handle errors in the coroutine that is launched after this
+        // Handle errors in the coroutine that is launched after this
         val errorHandler = CoroutineExceptionHandler {_, error ->
             CoroutineScope(Main).launch {
                 Toast.makeText(applicationContext, error.message ?: getString(R.string.network_error), Toast.LENGTH_LONG).show()
@@ -83,12 +92,55 @@ class BaseActivity : AppCompatActivity() {
             }
         }
 
-        //Launch a coroutine to display a message on sign in
+        // Launch a coroutine to display a message on sign in
         CoroutineScope(errorHandler).launch {
             val name = session.getName()
             withContext(Main) {
                 Toast.makeText(applicationContext, getString(R.string.signedIn_message) + name, Toast.LENGTH_SHORT).show()
             }
+        }
+
+        // Add listener for the "About" option in the menu
+        this.binding.menuItemAbout.setOnClickListener { v: View ->
+            // Close the navigation drawer
+            this.binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+            // Open the dialog after 250ms, to allow the navigation drawer's close animation to finish
+            Handler(Looper.getMainLooper()).postDelayed({
+                val builder = AlertDialog.Builder(this@BaseActivity)
+                val viewGroup = findViewById<ViewGroup>(android.R.id.content)
+                val dialogView = LayoutInflater.from(v.context).inflate(R.layout.dialog_about, viewGroup, false)
+
+                // Set dialog values
+                // Set version number
+                val pInfo = this.packageManager.getPackageInfo(this.packageName, 0)
+                dialogView.findViewById<TextView>(R.id.about_versionString).text = "${resources.getString(R.string.about_version_base)} ${pInfo.versionName}"
+
+                // Set website link
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    dialogView.findViewById<TextView>(R.id.textView_website).text =
+                        Html.fromHtml("<a href=\"${resources.getString(R.string.about_websiteUrl)}\">${resources.getString(R.string.about_website)}</a>", Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    dialogView.findViewById<TextView>(R.id.textView_website).text =
+                        Html.fromHtml("<a href=\"${resources.getString(R.string.about_websiteUrl)}\">${resources.getString(R.string.about_website)}</a>")
+                }
+                dialogView.findViewById<TextView>(R.id.textView_website).movementMethod = LinkMovementMethod.getInstance()
+
+                // Set source code link
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    dialogView.findViewById<TextView>(R.id.textView_source).text =
+                        Html.fromHtml("<a href=\"${resources.getString(R.string.about_githubUrl)}\">${resources.getString(R.string.about_source)}</a>", Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    dialogView.findViewById<TextView>(R.id.textView_source).text =
+                        Html.fromHtml("<a href=\"${resources.getString(R.string.about_githubUrl)}\">${resources.getString(R.string.about_source)}</a>")
+                }
+                dialogView.findViewById<TextView>(R.id.textView_source).movementMethod = LinkMovementMethod.getInstance()
+
+                // Open the dialog
+                builder.setView(dialogView)
+                val alertDialog = builder.create()
+                alertDialog.show()
+            }, 250)
         }
     }
 
